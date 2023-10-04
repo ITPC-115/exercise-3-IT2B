8410fdfb6dbed78b3c1888136a7d6886bc2f5c33
Rayveen John A. Escovidal <Escovidal-Ra@ndmu.local>
2023-10-04 11:25:51 +0800

    reeeeeeeb

--- Problem 1/problem1.js
+++ Problem 1/problem1.js
@@ -8,3 +8,52 @@
     Lotty 22 Female
         -- make sure that name and sex are strings and age is a number when displayed
 */
+
+const users =[
+    {
+        name: 'beK',
+        age: 22,
+        sex: 'Male',
+    },
+    {
+        name: 'Niel',
+        age: 23,
+        sex: 'Male',
+    },
+    {
+        name: 'Onnich',
+        age: 22,
+        sex: 'Male',
+    },
+    {
+        name: 'neD',
+        age: 22,
+        sex: 'Male',
+    },
+    {
+        name: 'Pranses',
+        age: 22,
+        sex: 'Male',
+    },
+    {
+        name: 'Jebus',
+        age: 22,
+        sex: 'Male',
+    },
+    {
+        name: 'Leng',
+        age: 22,
+        sex: 'Female',
+    },
+    {
+        name: 'beD',
+        age: 22,
+        sex: 'Male',
+    },
+    {
+        
+    }
+]
+for(let i = 0; i < users.length; i++){
+    console.log()
+}
