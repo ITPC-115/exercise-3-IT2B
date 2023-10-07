54f212d95a70a95f4571ff7f02c847b420dddd2d
Vincent Agustin <lerksflores@gmail.com>
2023-10-08 07:37:56 +0800

    muwehehehe

--- Problem 5/problem5.js
+++ Problem 5/problem5.js
@@ -7,3 +7,38 @@
     Constraints: The generated random number must be between 1 and 100 only.
 
 */
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+let guesser = false;
+let attempts = 0;
+
+while (!guesser)
+{
+    //const randomNumber = Math.floor(Math.random() * 100) + 1;
+    const userGuess = parseInt(prompt("Guess a number between 1 and 100:"));
+
+    if (userGuess < 1 || userGuess > 100 || isNaN(userGuess)) {
+        alert("Please enter a valid number between 1 and 100.");
+    } else {
+        attempts++;
+
+        if (userGuess == randomNumber) {
+            guesser = true;
+            console.log(`Attempt no: ${attempts}, User Guess: ${userGuess}, Answer: ${randomNumber}`);
+            alert(`Congratulations! You got it right.`);
+            console.log(`Congratulations! You got it right.`);
+        }
+        else if (userGuess < randomNumber)
+        {
+            console.log(`Attempt no: ${attempts}, User Guess: ${userGuess}, Answer: ${randomNumber}`);
+            alert(`Your Number: ${userGuess} is Wrong\n>>`);
+        }
+        
+        else
+        {
+            console.log(`Attempt no: ${attempts}, User Guess: ${userGuess}, Answer: ${randomNumber}`);
+            alert(`Your Number: ${userGuess} is Wrong\n<<`);
+        }
+    }
+}
