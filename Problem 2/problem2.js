18af0bae943b21508c330a899c28b2b146069430
Dondiego <you@example.com>
2023-10-14 03:50:07 +0800

    Hello

--- Problem 2/problem2.js
+++ Problem 2/problem2.js
@@ -5,6 +5,25 @@
     properties of the object. -- Make sure to incorprate the keyword this in here.
 */
 
-let 
+let Object  = {
+    name : "Honda Suzuki Guitar",
+    brand: "Honda",
+    price: 50000,
+    color: "Rainbow",
+};
+
+let x = Object;
+x.name = window.prompt("Enter the name: ");
+x.brand = prompt("Enter brand: ");
+x.price = prompt("Enter its price: ");
+x.color = prompt("Enter its color");
+
+DisplaythemAll_uwu(Object)
+
+function DisplaythemAll_uwu(Object){
+    let a = Object;
+    console.log("Name: ", a.name, '\n', 'Brand: ', a.brand, '\n', 'Price: ' , a.price, '\n', 'Color: ' , a.color);
+}
+//UPDATE
 
 
