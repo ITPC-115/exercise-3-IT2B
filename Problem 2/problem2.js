0aaa26564bfce8668da6ef131825ea9187399731
Earl Kristian Lance L. Ortiz <Ortiz-Ea@ndmu.local>
2023-10-14 12:06:13 +0800

    LANCE

--- Problem 2/problem2.js
+++ Problem 2/problem2.js
@@ -5,4 +5,16 @@
     properties of the object. -- Make sure to incorprate the keyword this in here.
 */
 
-
+let product = {
+    name: "Gaming laptop",
+    brand: "Acer",
+    price: "Php 30,350",
+    color: "Black",
+    displayProperties: function() {
+        console.log(`Name: ${this.name}`);
+        console.log(`Brand: ${this.brand}`)
+        console.log(`Price: ${this.price}`)
+        console.log(`Color: ${this.color}`)
+    }
+};
+product.displayProperties();
