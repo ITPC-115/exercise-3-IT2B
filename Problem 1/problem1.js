07ea116b2eff9ee572dfb480f591b0eb35b45765
Prince Jhame Paulo <Paulo-Pr@ndmu.local>
2023-11-11 10:05:45 +0800

    sorrylate

--- Problem 1/problem1.js
+++ Problem 1/problem1.js
@@ -8,3 +8,21 @@
     Lotty 22 Female
         -- make sure that name and sex are strings and age is a number when displayed
 */
+const person = [
+    { name: 'Saber', age: 24, sex: 'Male' },
+    { name: 'Layla', age: 27, sex: 'Female' },
+    { name: 'Mia', age: 22, sex: 'Female' },
+    { name: 'Alucard', age: 24, sex: 'Male' },
+    { name: 'Gord', age: 23, sex: 'Male' },
+    { name: 'Harry', age: 28, sex: 'Male' },
+    { name: 'Balmond', age: 25, sex: 'Male' },
+    { name: 'Karina', age: 27, sex: 'Female' },
+    { name: 'Grock', age: 23, sex: 'Female' },
+    { name: 'Kufhra', age: 26, sex: 'Female' },
+
+];
+
+user.forEach(function(person){
+    console.log(person.name + " " + person.age + " "+ person.sex);    
+    });
+    
